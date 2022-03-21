f14a389e59087b7ef58967352d6adc648f3abb08
monibbormon <monibbormon14@gmail.com>
2022-03-21 12:22:24 +0600

    project init and setup

---
+++ src/components/CryptoCurrencies.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CryptoCurrencies = () => {
+    return (
+        <div>
+            <h2>Crypto Currencies</h2>
+        </div>
+    );
+};
+
+export default CryptoCurrencies;
---
+++ src/components/CryptoDetails.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CryptoDetails = () => {
+    return (
+        <div>
+            <h2>crypto details</h2>
+        </div>
+    );
+};
+
+export default CryptoDetails;
